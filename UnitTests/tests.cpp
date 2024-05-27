a51636e407855b1771bf3c08107198901ee8cd9d
Urij19cc7e4 <urij19cc7e4@gmail.com>
2024-05-27 15:21:03 +0300

    arr renamed to vec

---
+++ UnitTests/tests.cpp
@@ -0,0 +1,13 @@
+#include <cstdint>
+#include <ctime>
+#include <random>
+
+#include "../NeuralNetwork/mtx.h"
+#include "../NeuralNetwork/vec.h"
+#include "../NeuralNetwork/cnn.h"
+#include "../NeuralNetwork/fnn.h"
+#include "../NeuralNetwork/rnn.h"
+#include "../NeuralNetwork/nnb.h"
+
+using namespace std;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
